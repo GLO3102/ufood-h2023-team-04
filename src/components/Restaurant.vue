aeb5fa68217d7f3ed5eee0ed628e9bd79fdf7cfc
Nabil <nabil.ouchaou.1@ulaval.ca>
2023-01-23 16:14:09 -0500

    premier test pour une page de resto

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,8 +1,37 @@
 <template>
   <div>
     <h1>Restaurant</h1>
-    <div>GLO-3102 Restaurant page</div>
+    <div class="Nom">
+      La belle et la boeuf
+    </div>
+    <div class="Image">
+      <img src="https://ssmscdn.yp.ca/image/resize/f21c276a-a02c-495c-bbd0-3cb5f146f3bf/ypui-d-mp-pic-gal-lg/la-belle-la-boeuf-burger-bar-laurier-devantdemagasin-6.jpg">
+    </div>
+    <div class="adresse">ceci est l'adresse</div>
+    
   </div>
 </template>
 
-<style></style>
+<style>
+.Nom {
+  display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    font-size: 50px;
+}
+
+.Image{
+  display: flex;
+  
+
+}
+
+.adresse {
+  display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    font-family: 'Times New Roman', Times, serif;
+}
+</style>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>User Profile</h1>
-    <div>GLO-3102 User Profile page</div>
+    <div>GLO-3102 Nabil</div>
   </div>
 </template>
 

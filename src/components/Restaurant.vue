5dcac9e6d36f4483df2f193c39b5ab9a02465960
Nabil <nabil.ouchaou.1@ulaval.ca>
2023-01-24 19:06:59 -0500

    ajout de map + organisation des balises

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,44 +1,56 @@
 <template>
-  <div>
-    <div class="h-56 grid grid-cols-3 gap-4 content-start">Restaurants</div>
-    <div class="flex items-start">
-      <div class="py-4, text-6xl , text-red-600, w-80">
-        La belle et la boeuf
-        <div class="adresse">ceci est l'adresse</div>
+  <div class="grid-cols-2 gap-4">
+    <div class="flex flex flex-row bg-green-100">
+      <div class="w-1/2 self-center">
+        <div class="text-5xl px-8">
+          <p class="text-5xl font-serif">La belle et la boeuf</p>
+        </div>
+
+        <div class="px-8">
+          <p class="">Adresse : 2450 Bd Laurier, Québec, QC G1V 2L1</p>
+          <p class="">Téléphone &nbsp; : (581) 742-5444</p>
+          <p class="underline underline-offset-1">Horaire</p>
+          <p class="">Samedi &emsp;&nbsp; :&emsp; 11h30-0h00</p>
+          <p class="">Dimanche &nbsp;: &emsp; 11h30-0h00</p>
+          <p class="">Lundi &emsp; : &emsp; 11h30-0h00</p>
+          <p class="">Mardi : &emsp; 11h30-0h00</p>
+          <p class="">Mercredi :&emsp; 11h30-0h00</p>
+          <p class="">Jeudi : &emsp;11h30-0h00</p>
+          <p class="">Vendredi :&emsp; 11h30-0h00</p>
+        </div>
+        <div class="pl-8 pt-6">
+          <button
+            class="flex flex-col items-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+          >
+            Site du restaurant
+          </button>
+        </div>
       </div>
-      <div class="py-12 , w-80">
+
+      <div class="w-1/2 self-center">
         <img
-          class="flex items-start"
+          class="self-center"
           src="https://ssmscdn.yp.ca/image/resize/f21c276a-a02c-495c-bbd0-3cb5f146f3bf/ypui-d-mp-pic-gal-lg/la-belle-la-boeuf-burger-bar-laurier-devantdemagasin-6.jpg"
         />
       </div>
     </div>
+
+    <div class="">
+      <div class="w-1/2">
+        <iframe
+          class="self-center h-50px w-30px"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10930.563052556623!2d-71.2801835!3d46.771977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdf8c41c7d41b0a7e!2sLa%20Belle%20et%20La%20B%C5%93uf!5e0!3m2!1sfr!2sca!4v1674599572955!5m2!1sfr!2sca"
+          width="720"
+          height="500"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <div class="w-1/2 h-1/2 bg-orange-100 sel justify-self-end"></div>
+    </div>
   </div>
 </template>
 
-<style>
-.DivResto {
-  display: flex;
-  flex-direction: row;
-}
-.Nom {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100px;
-  font-size: 50px;
-}
-
-.Image {
-  display: flex;
-  margin: 100px;
-}
-
-.adresse {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100px;
-  font-family: "Times New Roman", Times, serif;
-}
-</style>
+<style></style>

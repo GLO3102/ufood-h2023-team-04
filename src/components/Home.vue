2db7db4e05a09657a3cd12eaf1d25e5bf1d31670
Corrisback <philippe.corriveau.1@ulaval.ca>
2023-01-29 12:01:01 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,13 +9,13 @@
     <div>Restaurant Directory</div>
   </head>
   <body>
-    <div class= "flex items-center flex-col justify-between bg-green-50 mx-auto">
+    <div class= "flex items-center flex-col justify-between bg-green-50 mx-auto w-screen">
     <label
       for="Restaurant"
       class="flex items-center max-w-md mx-auto text-sm font-medium text-gray-700"
       >Looking for a restaurant?</label
     >
-    <div class="flex items-center max-w-md mx-auto mt-1 rounded-md shadow-sm">
+    <div class="flex items-center mx-auto mt-1 rounded-md shadow-sm w-1/4">
       <input
         type="text"
         name="name"
@@ -23,7 +23,7 @@
         class="block w-full rounded-md border-red-300 pl-7 pr-12 focus:border-green-500 focus:ring-indigo-500 sm:text-sm"
         placeholder="Enter the name of a restaurant"
       />
-      <div class="flex items-center max-w-md mx-auto inset-y-0 right-0">
+      <div class="flex items-center max-w-md mx-auto inset-y-0 right-0 w-fit bg-white rounded-md">
         <label for="categorie" class="srOnly"></label>
         <select
           id="categorie"
@@ -38,6 +38,7 @@
       </div>
     </div>
     <div class="flex items-center max-w-md mx-auto mt-1 rounded-md shadow-sm">
+      
       <ul
         class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400"
       >
@@ -45,12 +46,13 @@
           {{ restaurant.name }}
         </li>
       </ul>
+      
     </div>
     <div></div>
     </div>
   </body>
   <footer>
-    <div class="bg-gray-100">
+    <div class="bg-gray-100 absolute bottom-0 w-screen">
       <nav
         class="container py-4 mx-auto md:flex md:justify-between md:items-center"
       >
@@ -88,80 +90,10 @@
           <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
             <router-link to="/">Register</router-link>
           </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/">Sign in</router-link>
-          </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/">Deconnexion</router-link>
-          </li>
+
         </ul>
-      </nav>
-      
+      </nav>      
     </div>
-    <div class="bg-gray-100">
-      <nav
-        class="container py-4 mx-auto md:flex md:justify-between md:items-center"
-      >
-        <div class="flex items-center justify-between">
-          <h3 class="text-xl font-bold text-gray-800 md:text-2xl">
-            RestoChecker
-          </h3>
-          <!-- Mobile menu button -->
-          <div @click="showMenu = !showMenu" class="flex md:hidden">
-            <button type="button" class="text-gray-800 hover:text-gray-400">
-              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                <path
-                  fill-rule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <p>Copyright © 2023 RestauCheker</p>
-        </div>
-
-        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <ul
-          :class="showMenu ? 'flex' : 'hidden'"
-          class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
-        >
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/">Home</router-link>
-          </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/restaurant">Restaurant</router-link>
-          </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/user">User Profile</router-link>
-          </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/">Register</router-link>
-          </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/">Sign in</router-link>
-          </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
-            <router-link to="/">Deconnexion</router-link>
-          </li>
-        </ul>
-      </nav>
-      <div class="flex flex-row justify-end pb-2">
-        <input
-          class="border-2 border-gray-300 bg-white h-10 px-2 rounded-lg text-sm focus:outline-none"
-          type="search"
-        />
-        <button
-          type="submit"
-          class="border-2 border-gray-300 bg-white h-10 px-2 rounded-lg text-sm focus:outline-none bg-blue-400 hover:bg-blue-800"
-        >
-          search
-        </button>
-      </div>
-        <p>Copyright © 2023 RestauCheker</p>
-    </div>
-
-
-
   </footer>
 </template>
 

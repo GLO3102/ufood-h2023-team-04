1d4890f021af65ccafd800518a226d36df520838
= <=>
2023-01-30 18:35:42 -0500

    Reworked routing, fixed user display and gunned prettier

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,13 +2,13 @@
   <nav>
     <ul>
       <li>
-        <router-link to="/">Home</router-link>
+        <router-link :to="{name: 'Home'}">Home</router-link>
       </li>
       <li>
-        <router-link to="/restaurant">Restaurant</router-link>
+        <router-link :to="{name: 'Restaurant'}">Restaurant</router-link>
       </li>
       <li>
-        <router-link to="/user">User Profile</router-link>
+        <router-link :to="{name:'User', params: {currentUserID: '1'}}">User Profile</router-link>
       </li>
     </ul>
   </nav>

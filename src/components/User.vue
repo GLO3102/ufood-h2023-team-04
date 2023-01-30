7866b0b0fa90d111d64508392c0b07990fe0c47d
David <David_cote_09@hotmail.com>
2023-01-30 13:57:55 -0500

    Ajustement

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,7 @@
 <template>
-  <body class="bg-gray-300 antialiased">
+  <body
+    class="bg-gradient-to-r from-sky-500 to-indigo-500 to-pink-500 antialiased"
+  >
     <div class="container mx-auto my-60">
       <div>
         <div
@@ -13,48 +15,44 @@
             />
           </div>
 
-          <div class="mt-16">
-            <h1 class="font-bold text-center text-3xl text-gray-900">
-              David Côté
-            </h1>
-            <p class="text-center text-sm text-gray-400 font-medium">
-              UI Components Factory
-            </p>
-            <p>
-              <span> </span>
+          <div class="mt-16" v-for="user in users.slice(0, 1)" :key="user.id">
+            <h1 class="font-bold text-center text-3xl text-gray-900"></h1>
+            <p class="text-center">
+              <span> {{ user.fullname }} </span>
             </p>
             <div class="flex justify-between items-center my-5 px-6">
               <a
                 class="text-gray-500 bg-gray-100 rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3"
-                >69 points</a
+                >Score: {{ user.score }}</a
               >
             </div>
-            <div v-if="visit" class="w-full">
-              <h3 class="font-medium text-gray-900 text-left px-6">
-                Recent visit
-              </h3>
-              <div
-                class="mt-5 w-full flex flex-col items-center overflow-hidden text-sm"
-              >
-                <a
-                  href="#"
-                  class="w-full border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150"
+            <div v-if="visit">
+              <div class="w-full">
+                <h3 class="font-medium text-gray-900 text-left px-6">
+                  Recent visit
+                </h3>
+                <div
+                  class="mt-5 w-full flex flex-col items-center overflow-hidden text-sm"
+                  v-for="user in users.slice(0, 1)"
+                  :key="user.id"
                 >
-                  <img
-                    src="https://avatars0.githubusercontent.com/u/35900628?v=4"
-                    alt=""
-                    class="rounded-full h-6 shadow-md inline-block mr-2"
-                  />
-                  La belle et la boeuf
-                </a>
+                  <a
+                    href="#"
+                    class="w-full border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150"
+                  >
+                    <img
+                      src="https://avatars0.githubusercontent.com/u/35900628?v=4"
+                      alt=""
+                      class="rounded-full h-6 shadow-md inline-block mr-2"
+                    />
+                    {{ user.visit }}
+                  </a>
+                </div>
               </div>
             </div>
-            <div>
-              <li>
+            <div class="grid justify-items-center">
+              <li v-if="!visit">
                 <router-link to="/">Home</router-link>
-                <div v-for="user in users.slice(0, 1)" :key="user.id">
-                  {{ user.id }} {{ user.fullname }} {{ user.score }}
-                </div>
               </li>
             </div>
           </div>
@@ -69,7 +67,7 @@ import MY_JSON from "@/data/users.json";
 export default {
   data() {
     return {
-      visit: false,
+      visit: true,
       users: MY_JSON,
     };
   },

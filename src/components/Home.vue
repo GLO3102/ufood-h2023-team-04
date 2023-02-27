bc6a917fe0372f962326bbddd1fd25d941cf369c
Corrisback <>
2023-02-27 13:05:33 -0500

    merged

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,58 +18,56 @@
           v-model="filterText"
         />
 
-            <label for="categorie" class="srOnly"></label>
-            <select
-              id="categorie"
-              name="categorie"
-              class="h-full rounded-md border bg-transparent py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
-            >
-              <option value="any">Any type</option>
-              <option value="ame">American</option>
-              <option value="ita">Italian</option>
-              <option value="sea">Seafood</option>
-              <option value="jap">Japanese</option>
-            </select>
-            <select
-              name="price"
-              id="price"
-              class="h-full rounded-md border py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
-            >
-              <option value="billGates">Any price</option>
-              <option value="poor">$</option>
-              <option value="lessPoor">$$</option>
-              <option value="notPoor">$$$</option>
-              <option value="rich">$$$$</option>
-            </select>
-            <button
-              id="search"
-              type="submit"
-              class="h-full rounded py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="container w-full ms:w-60">
-        <div
-          class="flex justify-center text-left mt-1 rounded-md shadow-sm bg-neutral-800"
+        <label for="categorie" class="srOnly"></label>
+        <select
+          id="categorie"
+          name="categorie"
+          class="h-full rounded-md border bg-transparent py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
         >
-          <ul class="w-80 space-y-1 text-neutral-100 m-3">
-            <li
-              class="flex flex-col justify-center text-center border border-neutral-700 rounded bg-neutral-700"
-              v-for="restaurant in restaurant"
-              :key="restaurant.name"
-            >
-              {{ restaurant.name }}
-              <div class="flex items-left flex-col bg-neutral-600">
-                <span>Price {{ restaurant.price }} </span>
-                <span>Ratings {{ restaurant.rating }}</span>
-                <span>Type {{ restaurant.type }}</span>
-              </div>
-            </li>
-          </ul>
-        </div>
+          <option value="any">Any type</option>
+          <option value="ame">American</option>
+          <option value="ita">Italian</option>
+          <option value="sea">Seafood</option>
+          <option value="jap">Japanese</option>
+        </select>
+        <select
+          name="price"
+          id="price"
+          class="h-full rounded-md border py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
+        >
+          <option value="billGates">Any price</option>
+          <option value="poor">$</option>
+          <option value="lessPoor">$$</option>
+          <option value="notPoor">$$$</option>
+          <option value="rich">$$$$</option>
+        </select>
+        <button
+          id="search"
+          type="submit"
+          class="h-full rounded py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+    <div class="container w-full ms:w-60">
+      <div
+        class="flex justify-center text-left mt-1 rounded-md shadow-sm bg-neutral-800"
+      >
+        <ul class="w-80 space-y-1 text-neutral-100 m-3">
+          <li
+            class="flex flex-col justify-center text-center border border-neutral-700 rounded bg-neutral-700"
+            v-for="(restaurant, index) in filteredRestaurants"
+            :key="index"
+          >
+            {{ restaurant.name }}
+            <div class="flex items-left flex-col bg-neutral-600">
+              <span>Price {{ restaurant.price }} </span>
+              <span>Ratings {{ restaurant.rating.toFixed(1) }}</span>
+              <span>Type {{ restaurant.genres.join(", ") }}</span>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </body>

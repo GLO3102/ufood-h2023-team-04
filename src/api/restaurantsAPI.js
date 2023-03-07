624d457d6f41a2d6c7c1c63b6981c3cdecb5b16e
Francis Descotes <francis.descotes.1@ulaval.ca>
2023-03-07 15:15:47 -0500

    filters are WOOOORKIIIING

--- src/api/restaurantsAPI.js
+++ src/api/restaurantsAPI.js
@@ -1,17 +1,19 @@
 const ENDPOINT = "https://ufoodapi.herokuapp.com/unsecure";
 
-export const getRestoNames = async () => {
+export const getRestaurants = async () => {
   try {
     const response = await fetch(`${ENDPOINT}/restaurants`);
     const data = await response.json();
-    const restaurants = data.items.slice(0, 9).map((item) => {
+    const test = data.items;
+    const restaurants = data.items.slice().map((item) => {
       return {
         name: item.name,
         rating: item.rating,
         genres: item.genres,
-        price: item.price_range,
+        price: item.price_range.toString(),
       };
     });
+    console.log(test);
     return restaurants;
   } catch (error) {
     console.error("can't fecth restaurants names", error);
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,57 +20,51 @@
 
         <label for="categorie" class="srOnly"></label>
         <select
+          v-model="selectGenre"
           id="categorie"
           name="categorie"
           class="h-full rounded-md border bg-transparent py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
         >
-          <option value="any">Any type</option>
-          <option
-            v-for="restaurant in restaurants"
-            :key="restaurant"
-            :value="restaurant.genres"
-          >
-            {{ restaurant.genres }}
+          <option value="Any Type" selected>Any type</option>
+          <option v-for="genre in AllGenres" :key="genre">
+            {{ genre }}
           </option>
-          <!-- <option value="any">Any type</option>
-          <option value="ame">American</option>
-          <option value="ita">Italian</option>
-          <option value="sea">Seafood</option>
-          <option value="jap">Japanese</option> -->
         </select>
         <select
+          v-model="selectPrice"
           name="price"
           id="price"
           class="h-full rounded-md border py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
         >
-          <option value="billGates">Any price</option>
-          <option value="poor">$</option>
-          <option value="lessPoor">$$</option>
-          <option value="notPoor">$$$</option>
-          <option value="rich">$$$$</option>
+          <option value="Any Price">Any price</option>
+          <option value="1">$</option>
+          <option value="2">$$</option>
+          <option value="3">$$$</option>
+          <option value="4">$$$$</option>
         </select>
-        <button
+        <!-- <button
           id="search"
           type="submit"
           class="h-full rounded py-0 pl-2 pr-7 text-neutral-800 focus:border-blue-400 focus:ring-blue-400 sm:text-sm"
         >
           Search
-        </button>
+        </button> -->
       </div>
     </div>
     <div class="container w-full ms:w-60">
       <div
         class="flex justify-center text-left mt-1 rounded-md shadow-sm bg-neutral-800"
       >
+        <!-- <p>{{ restaurantsNames }}</p> -->
         <ul class="w-80 space-y-1 text-neutral-100 m-3">
           <li
             class="flex flex-col justify-center text-center border border-neutral-700 rounded bg-neutral-700"
-            v-for="(restaurant, index) in filteredRestaurants"
-            :key="index"
+            v-for="restaurant in filteredRestaurants"
+            :key="restaurant"
           >
             {{ restaurant.name }}
             <div class="flex items-left flex-col bg-neutral-600">
-              <span>Price {{ restaurant.price }} </span>
+              <span>Price {{ "$".repeat(restaurant.price) }} </span>
               <span>Ratings {{ restaurant.rating.toFixed(1) }}</span>
               <span>Type {{ restaurant.genres.join(", ") }}</span>
             </div>
@@ -82,29 +76,65 @@
 </template>
 
 <script>
-import { getRestoNames } from "@/api/restaurantsAPI.js";
+import { getRestaurants } from "@/api/restaurantsAPI.js";
+import { ref, computed, watch } from "vue";
 
 export default {
   name: "Home",
-  data() {
-    return {
-      restaurants: [],
-      filterText: "",
+  setup() {
+    const AllRestaurants = ref([]);
+    const AllGenres = ref([]);
+    const filterText = ref("");
+    const selectGenre = ref("Any Type");
+    const selectPrice = ref("Any Price");
+
+    watch(selectGenre, (newValue, oldValue) => {
+      console.log(`Selected option changed to ${newValue}`);
+    });
+
+    watch(selectPrice, (newValue, oldValue) => {
+      console.log(`Selected option changed to ${newValue}`);
+    });
+
+    const extractRestos = async () => {
+      AllRestaurants.value = await getRestaurants();
+      const allGenres = AllRestaurants.value.flatMap(
+        (restaurant) => restaurant.genres
+      );
+      AllGenres.value = [...new Set(allGenres)];
+      console.log(AllGenres.value);
     };
-  },
-  async created() {
-    this.restaurants = await getRestoNames();
-  },
-  computed: {
-    filteredRestaurants() {
-      if (!this.filterText) {
-        return this.restaurants;
-      } else {
-        return this.restaurants.filter((restaurant) =>
-          restaurant.name.toLowerCase().includes(this.filterText.toLowerCase())
+
+    const filteredRestaurants = computed(() => {
+      let filtered = AllRestaurants.value;
+      if (filterText.value) {
+        filtered = filtered.filter((restaurant) =>
+          restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
         );
       }
-    },
+      if (selectGenre.value !== "Any Type") {
+        filtered = filtered.filter((restaurant) =>
+          restaurant.genres.includes(selectGenre.value)
+        );
+      }
+      if (selectPrice.value !== "Any Price") {
+        filtered = filtered.filter((restaurant) =>
+          restaurant.price.includes(selectPrice.value)
+        );
+      }
+      return filtered;
+    });
+
+    extractRestos();
+
+    return {
+      AllRestaurants,
+      AllGenres,
+      filteredRestaurants,
+      filterText,
+      selectGenre,
+      selectPrice,
+    };
   },
 };
 </script>

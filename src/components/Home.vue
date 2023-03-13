eb2b606e3bf02303dadc3d83b03848f67f2e1ae7
Corrisback <>
2023-03-13 14:10:46 -0400

    pagination

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,25 @@
+<template>
+  <div>
+    <FilterRestaurants
+      @filtering="restaurantsFiltered"
+      v-if="isloaded"
+      :restaurants="json"
+    />
+    <Restaurants :restaurants="displayedRestaurants" v-if="isloaded" />
+    <Pagination
+      v-if="isloaded"
+      :items="resoFiltered"
+      v-model="currentPageProxy"
+      :num-pages="numPages"
+      @page-changed="changePage"
+    />
+  </div>
+</template>
+
 <script setup>
 import Restaurants from "@/components/home/Restaurants.vue";
 import { getRestaurants } from "@/composable/UseRestaurant";
-import { ref, computed } from "vue";
+import { ref, computed, watch, defineEmits } from "vue";
 import FilterRestaurants from "@/components/home/FilterRestaurants.vue";
 import Pagination from "@/components/home/Pagination.vue";
 
@@ -9,52 +27,50 @@ const json = ref(null);
 const isloaded = ref(false);
 const resoFiltered = ref(null);
 const itemsPerPage = 10;
-let numPages = 0;
-let currentPage = 1;
+const currentPage = ref(1);
+const numPages = ref(1);
 
 const fetchData = async () => {
   const data = await getRestaurants();
   json.value = data.items;
   isloaded.value = true;
   resoFiltered.value = json.value;
-  numPages = Math.ceil(json.value.length / itemsPerPage);
+  numPages.value = Math.ceil(json.value.length / itemsPerPage);
 };
 
 const restaurantsFiltered = (filteredRestaurants) => {
   resoFiltered.value = filteredRestaurants;
-  numPages = Math.ceil(resoFiltered.value.length / itemsPerPage);
-  currentPage = 1;
+  currentPage.value = 1;
 };
 
+const emits = defineEmits(["page-changed", "update:currentPage"]);
+
 const changePage = (page) => {
-  currentPage = page;
+  currentPage.value = page;
+  emits("page-changed", currentPage.value);
+  emits("update:currentPage", currentPage.value);
 };
 
 const displayedRestaurants = computed(() => {
-  const start = (currentPage - 1) * itemsPerPage;
+  const start = (currentPageProxy.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
   return resoFiltered.value.slice(start, end);
 });
 
+watch(resoFiltered, () => {
+  currentPage.value = 1;
+});
+
+const currentPageProxy = computed({
+  get() {
+    return currentPage.value;
+  },
+  set(value) {
+    currentPage.value = value;
+  },
+});
+
 fetchData();
 </script>
-
-<template>
-  <div>
-    <FilterRestaurants
-      @filtering="restaurantsFiltered"
-      v-if="isloaded"
-      :restaurants="json"
-    ></FilterRestaurants>
-    <Restaurants :restaurants="resoFiltered" v-if="isloaded"></Restaurants>
-    <Pagination
-      v-if="isloaded"
-      :items="resoFiltered"
-      :current-page-proxy="currentPageProxy"
-      :num-pages="numPages"
-      @page-changed="currentPageProxy = $event"
-    ></Pagination>
-  </div>
-</template>
 
 <style></style>
--- src/components/home/Pagination.vue
+++ src/components/home/Pagination.vue
@@ -1,9 +1,26 @@
 <template>
-  <v-pagination
-    v-model="currentPageProxy"
-    :length="numPages"
-    @input="changePage"
-  />
+  <div class="pagination">
+    <button
+      class="previous"
+      :disabled="currentPageProxy === 1"
+      @click="currentPageProxy--"
+    >
+      Previous
+    </button>
+    <v-pagination
+      v-if="numPages > 1"
+      v-model="currentPageProxy"
+      :length="numPages"
+      @input="changePage"
+    />
+    <button
+      class="next"
+      :disabled="currentPageProxy === numPages"
+      @click="currentPageProxy++"
+    >
+      Next
+    </button>
+  </div>
 </template>
 
 <script setup>
@@ -11,8 +28,34 @@ import { computed, defineProps, defineEmits } from "vue";
 
 const itemsPerPage = 10;
 
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+  currentPage: {
+    type: Number,
+    required: true,
+  },
+  "onUpdate:currentPage": {
+    type: Function,
+    required: true,
+  },
+});
+
+const emits = defineEmits(["page-changed", "update:currentPage"]);
+
+const currentPageProxy = computed({
+  get() {
+    return props.currentPage;
+  },
+  set(value) {
+    props["onUpdate:currentPage"](value);
+  },
+});
+
 const displayedItems = computed(() => {
-  const start = (props.currentPage - 1) * itemsPerPage;
+  const start = (currentPageProxy.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
   return props.items.slice(start, end);
 });
@@ -26,29 +69,41 @@ const changePage = (page) => {
   emits("update:currentPage", page);
 };
 
-const props = defineProps({
-  items: {
-    type: Array,
-    required: true,
-  },
-  currentPage: {
-    type: Number,
-    required: true,
-  },
-});
+const nextPage = () => {
+  if (currentPage.value < numPages.value) {
+    currentPage.value++;
+  }
+};
 
-const emits = defineEmits(["page-changed", "update:currentPage"]);
-
-const currentPageProxy = computed({
-  get() {
-    return props.currentPage;
-  },
-  set(value) {
-    emits("update:currentPage", value);
-  },
-});
+const previousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+};
 </script>
 
 <style scoped>
-/* Add any custom styles you need here */
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
+
+.previous,
+.next {
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.previous: disabled, .next;
 </style>

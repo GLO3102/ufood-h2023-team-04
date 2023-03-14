ae3f512850d421744f7bfac17166a9ea3b9bbfc4
Nabil <nabil.ouchaou.1@ulaval.ca>
2023-03-14 17:10:29 -0400

    implementation de la page des resto visités

---
+++ src/components/VisitedRestaurants.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <FilterRestaurants
+      @filtering="restaurantsFiltered"
+      v-if="isloaded"
+      :restaurants="json"
+    />
+
+    <Pagination
+      v-if="isloaded"
+      :items="visitedRestoFormate"
+      :num-pages="numPages"
+      :current-page="currentPage"
+      :on-update-current-page="(page) => (currentPage = page)"
+      @update:currentPage="changePage"
+    />
+    <Restaurants :restaurants="visitedRestoFormate"></Restaurants>
+    <Pagination
+      v-if="isloaded"
+      :items="visitedRestoFormate"
+      :num-pages="numPages"
+      :current-page="currentPage"
+      :on-update-current-page="(page) => (currentPage = page)"
+      @update:currentPage="changePage"
+    />
+  </div>
+</template>
+
+<script setup>
+import Restaurants from "@/components/home/Restaurants.vue";
+import { getRestaurant, getRestaurants, getVisitedRestaurentsByUser } from "@/composable/UseRestaurant";
+import { ref, computed, watch, defineEmits } from "vue";
+import FilterRestaurants from "@/components/home/FilterRestaurants.vue";
+import Pagination from "@/components/home/Pagination.vue";
+
+const json = ref(null);
+const isloaded = ref(false);
+const resoFiltered = ref(null);
+const itemsPerPage = 10;
+const currentPage = ref(1);
+const numPages = ref(1);
+let idsOfVisitedResto = ref(null);
+let visitedRestoFormate = ref(null);
+const fetchData = async () => {
+  const data = await getRestaurants();
+  json.value = data.items;
+  isloaded.value = true;
+  resoFiltered.value = json.value;
+  numPages.value = Math.ceil(json.value.length / itemsPerPage);
+};
+const getData = async () => {
+  const info = await getVisitedRestaurentsByUser();
+  idsOfVisitedResto.value = info.items;
+  visitedRestoFormate.value = await formatRestaurents(idsOfVisitedResto.value);
+};
+const formatRestaurents = async function (visitedResto) {
+  const listeDeRestoFormate = [];
+  for (const element of visitedResto) {
+    listeDeRestoFormate.push(await getRestaurant(element["restaurant_id"]));
+  }
+  return listeDeRestoFormate;
+};
+
+const emits = defineEmits(["update:currentPage"]);
+
+const restaurantsFiltered = (filteredRestaurants) => {
+  resoFiltered.value = filteredRestaurants;
+  currentPage.value = 1;
+};
+
+watch(resoFiltered, () => {
+  currentPage.value = 1;
+});
+const changePage = (page) => {
+  currentPage.value = page;
+};
+
+fetchData();
+getData();
+</script>
+
+<style></style>

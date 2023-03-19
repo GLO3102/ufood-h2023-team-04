3f91386ff717a37646d68c64e41cd5981938fa1c
yacine Kahlis <yacine.kahlis.1@ulaval.ca>
2023-03-19 12:52:54 -0400

    reparation du code

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,58 +1,46 @@
 <template>
-  <v-container class="mt-5">
-    <v-toolbar :elevation="0" color="white">
-      <v-toolbar-title>RestoCheker</v-toolbar-title>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-responsive class="mx-auto" min-width="200px">
-          <v-text-field
-            v-if="searchIsOpen"
-            hide-details
-            prepend-icon="mdi-magnify"
-            single-line
-            min-width="100px"
-          ></v-text-field>
-        </v-responsive>
-        <v-btn icon @click="searchIsOpen = !searchIsOpen">
-          <font-awesome-icon icon="fa-solid fa-user" />
-        </v-btn>
-        <v-btn>
-          <router-link :to="'/'">Home</router-link>
-        </v-btn>
-        <v-btn @click="loggedIn = true" v-if="loggedIn === false"
-          >Register</v-btn
+  <v-container>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click="drawer = true"> </v-app-bar-nav-icon>
+      <v-toolbar-title>RestoAnalyser3000</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
         >
-        <v-btn @click="loggedIn = false" v-if="loggedIn === true"
-          >log out</v-btn
-        >
-        <v-btn>
-          <router-link
-            :to="{
-              name: 'User',
-              params: { currentUserID: '604cc220ef6fa10004dc0179' },
-            }"
-          >
-            User Profile
-          </router-link>
-        </v-btn>
-        <v-btn v-if="!loggedIn">Sign in</v-btn>
-      </v-toolbar-items>
-
-      <div class="hidden-sm-and-up">
-        <v-menu :location="location">
-          <template v-slot:activator="{ props }">
-            <v-btn color="primary" dark v-bind="props"> Dropdown </v-btn>
-          </template>
-
-          <v-list class="reponsiveMenu">
-            <v-list-item>
-              <v-list-item-title>
-                <router-link :to="'/'">Home</router-link></v-list-item-title
-              >
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
-    </v-toolbar>
+          <v-list-item>
+            <router-link :to="'/'">Home</router-link>
+          </v-list-item>
+          <v-list-item>
+            <router-link
+              :to="{
+                name: 'User',
+                params: { currentUserID: '604cc220ef6fa10004dc0179' },
+              }"
+            >
+              User Profile
+            </router-link>
+          </v-list-item>
+          <v-list-item v-if="loggedIn === false">
+            <router-link :to="'/'" @click="loggedIn = true"
+              >Register</router-link
+            >
+          </v-list-item>
+          <v-list-item v-if="loggedIn">
+            <router-link :to="'/'" @click="loggedIn = true"
+              >Sign in</router-link
+            >
+          </v-list-item>
+          <v-list-item v-if="loggedIn === true">
+            <router-link :to="'/'" @click="loggedIn = false"
+              >log out
+            </router-link>
+          </v-list-item>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-container>
 </template>
 <script setup>
@@ -60,15 +48,8 @@ import { ref } from "vue";
 
 let loggedIn = ref(false);
 let searchIsOpen = ref(false);
+const drawer = ref(false);
+const group = ref(null);
 </script>
 
-<style scoped>
-.v-container {
-  background: white;
-  color: #fff;
-  font-family: "Helvetica Neue", Arial, sans-serif;
-  padding: 20px;
-  border: 3px solid #000;
-  box-shadow: 10px 15px 0px black;
-}
-</style>
+<style scoped></style>

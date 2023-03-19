36e1bea92cdf7254589475a1449bc278c055bec3
Nabil <nabil.ouchaou.1@ulaval.ca>
2023-03-19 19:31:21 -0400

    navbar mieux foutu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,58 +1,77 @@
 <template>
   <v-container class="mt-5">
-    <v-toolbar :elevation="0" color="white">
-      <v-toolbar-title>RestoCheker</v-toolbar-title>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-responsive class="mx-auto" min-width="200px">
-          <v-text-field
-            v-if="searchIsOpen"
-            hide-details
-            prepend-icon="mdi-magnify"
-            single-line
-            min-width="100px"
-          ></v-text-field>
-        </v-responsive>
-        <v-btn icon @click="searchIsOpen = !searchIsOpen">
-          <font-awesome-icon icon="fa-solid fa-user" />
-        </v-btn>
-        <v-btn>
-          <router-link :to="'/'">Home</router-link>
-        </v-btn>
-        <v-btn @click="loggedIn = true" v-if="loggedIn === false"
-          >Register</v-btn
-        >
-        <v-btn @click="loggedIn = false" v-if="loggedIn === true"
-          >log out</v-btn
-        >
-        <v-btn>
-          <router-link
-            :to="{
-              name: 'User',
-              params: { currentUserID: '604cc220ef6fa10004dc0179' },
-            }"
+    <div class="hidden-md-and-down">
+      <v-toolbar :elevation="0" color="white">
+        <v-toolbar-title>RestoCheker</v-toolbar-title>
+        <v-text-field></v-text-field>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn>
+            <router-link :to="'/'">Home</router-link>
+          </v-btn>
+          <v-btn @click="loggedIn = true" v-if="loggedIn === false"
+            >Register</v-btn
           >
-            User Profile
-          </router-link>
-        </v-btn>
-        <v-btn v-if="!loggedIn">Sign in</v-btn>
-      </v-toolbar-items>
+          <v-btn @click="loggedIn = false" v-if="loggedIn === true"
+            >log out</v-btn
+          >
+          <v-btn>
+            <router-link
+              v-if="loggedIn"
+              :to="{
+                name: 'User',
+                params: { currentUserID: '604cc220ef6fa10004dc0179' },
+              }"
+            >
+              User Profile
+            </router-link>
+          </v-btn>
+          <v-btn v-if="!loggedIn">Sign in</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
 
-      <div class="hidden-sm-and-up">
-        <v-menu :location="location">
+    <div class="hidden-lg-and-up">
+      <v-toolbar>
+        <v-toolbar-title>RestoCheker</v-toolbar-title>
+        <v-menu>
           <template v-slot:activator="{ props }">
-            <v-btn color="primary" dark v-bind="props"> Dropdown </v-btn>
+            <v-btn v-bind="props"> Menu </v-btn>
           </template>
-
           <v-list class="reponsiveMenu">
             <v-list-item>
               <v-list-item-title>
-                <router-link :to="'/'">Home</router-link></v-list-item-title
+                <v-layout @click="loggedIn = true" v-if="loggedIn === false"
+                  >Register</v-layout
+                ></v-list-item-title
+              >
+              <v-list-item-title>
+                <v-layout>
+                  <router-link :to="'/'">Home</router-link>
+                </v-layout></v-list-item-title
+              >
+              <v-list-item-title>
+                <v-layout>
+                  <router-link
+                    v-if="loggedIn"
+                    :to="{
+                      name: 'User',
+                      params: { currentUserID: '604cc220ef6fa10004dc0179' },
+                    }"
+                  >
+                    User Profile
+                  </router-link>
+                </v-layout></v-list-item-title
+              >
+              <v-list-item-title>
+                <v-layout @click="loggedIn = false" v-if="loggedIn === true">
+                  log out
+                </v-layout></v-list-item-title
               >
             </v-list-item>
           </v-list>
         </v-menu>
-      </div>
-    </v-toolbar>
+      </v-toolbar>
+    </div>
   </v-container>
 </template>
 <script setup>
@@ -70,5 +89,13 @@ let searchIsOpen = ref(false);
   padding: 20px;
   border: 3px solid #000;
   box-shadow: 10px 15px 0px black;
+}
+v-menu {
+  background-color: white;
+}
+
+v-text-field {
+  display: flex;
+  justify-self: center;
 }
 </style>

b1d47ff9dcef35373470d95218846bcfa778b5a5
Nabil <nabil.ouchaou.1@ulaval.ca>
2023-03-23 15:34:29 -0400

    navbar stylé

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,20 +3,18 @@
     <div class="hidden-md-and-down">
       <v-toolbar :elevation="0" color="white">
         <v-toolbar-title>RestoCheker</v-toolbar-title>
-        <v-text-field></v-text-field>
-        <v-toolbar-items class="hidden-xs-only">
-          <v-btn>
+        <v-toolbar-items class="hidden-xs-only"
+          ><v-btn>
             <router-link :to="'/'">Home</router-link>
           </v-btn>
           <v-btn @click="loggedIn = true" v-if="loggedIn === false"
-            >Register</v-btn
+            >Sign in</v-btn
           >
           <v-btn @click="loggedIn = false" v-if="loggedIn === true"
-            >log out</v-btn
-          >
-          <v-btn>
+            ><router-link :to="'/'">log out </router-link>
+          </v-btn>
+          <v-btn v-if="loggedIn">
             <router-link
-              v-if="loggedIn"
               :to="{
                 name: 'User',
                 params: { currentUserID: '604cc220ef6fa10004dc0179' },
@@ -25,29 +23,29 @@
               User Profile
             </router-link>
           </v-btn>
-          <v-btn v-if="!loggedIn">Sign in</v-btn>
+          <v-btn v-if="!loggedIn">Register</v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </div>
 
     <div class="hidden-lg-and-up">
-      <v-toolbar>
+      <v-toolbar :elevation="0" color="white">
         <v-toolbar-title>RestoCheker</v-toolbar-title>
-        <v-menu>
+        <v-menu color="white">
           <template v-slot:activator="{ props }">
             <v-btn v-bind="props"> Menu </v-btn>
           </template>
           <v-list class="reponsiveMenu">
             <v-list-item>
               <v-list-item-title>
-                <v-layout @click="loggedIn = true" v-if="loggedIn === false"
-                  >Register</v-layout
-                ></v-list-item-title
-              >
-              <v-list-item-title>
                 <v-layout>
                   <router-link :to="'/'">Home</router-link>
                 </v-layout></v-list-item-title
+              >
+              <v-list-item-title>
+                <v-layout @click="loggedIn = true" v-if="loggedIn === false"
+                  >Sign in</v-layout
+                ></v-list-item-title
               >
               <v-list-item-title>
                 <v-layout>
@@ -64,8 +62,13 @@
               >
               <v-list-item-title>
                 <v-layout @click="loggedIn = false" v-if="loggedIn === true">
-                  log out
+                  <router-link :to="'/'">log out </router-link>
                 </v-layout></v-list-item-title
+              >
+              <v-list-item-title>
+                <v-layout v-if="loggedIn === false"
+                  >Register</v-layout
+                ></v-list-item-title
               >
             </v-list-item>
           </v-list>

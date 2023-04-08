b687d302471e1e9752e144e7fa9b7152e8a7cb79
David <David_cote_09@hotmail.com>
2023-04-08 12:22:37 -0400

    Card pas pire

--- src/components/users/UserCard.vue
+++ src/components/users/UserCard.vue
@@ -1,41 +1,35 @@
 <template>
   <v-container>
     <v-row justify="space-around">
-      <v-card width="300">
-        <v-img
-          height="200"
-          src="https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590_960_720.jpg"
-          cover
-          class="text-white"
-        >
-          <v-toolbar color="rgba(0, 0, 0, 0)" theme="dark">
-            <template v-slot:prepend>
-              <v-btn icon="$menu"></v-btn>
-            </template>
+      <v-card width="300" color="blue-lighten-1">
+        <v-toolbar color="blue-darken-2">
+          <template v-slot:prepend>
+            <v-btn icon="$menu"></v-btn>
+          </template>
 
-            <v-toolbar-title class="text-h6"> {{ id.name }} </v-toolbar-title>
+          <v-toolbar-title class="text-h6">
+            {{ id.name }}
+          </v-toolbar-title>
 
-            <template v-slot:append>
-              <v-btn icon="mdi-dots-vertical"></v-btn>
-            </template>
-          </v-toolbar>
-        </v-img>
+          <template v-slot:append
+            ><v-btn
+              class="ma-2"
+              color="black"
+              icon="mdi-account-plus-outline"
+            ></v-btn>
+          </template>
+        </v-toolbar>
 
         <v-card-text>
-          <div class="font-weight-bold ms-1 mb-2">Today</div>
+          <div class="font-weight-bold ms-1 mb-2">Recent Visits</div>
 
           <v-timeline density="compact" align="start">
-            <v-timeline-item
-              v-for="message in messages"
-              :key="message.time"
-              :dot-color="message.color"
-              size="x-small"
-            >
+            <v-timeline-item>
               <div class="mb-4">
                 <div class="font-weight-normal">
-                  <strong>{{ message.from }}</strong> @{{ message.time }}
+                  <strong> message.from </strong> @ message.time
                 </div>
-                <div>{{ message.message }}</div>
+                <div>message.message</div>
               </div>
             </v-timeline-item>
           </v-timeline>
@@ -55,8 +49,17 @@ const props = defineProps({
 });
 
 const info = ref([]);
+const visits = ref([]);
 
 const getInfo = async () => {
   info.value = await getUserInfo(props.id);
+};
+
+const getVisits = async () => {
+  visits.value = await getUserVisits();
+  visits.value.forEach(async (visit) => {
+    const name = await getRestaurantsNameByID(visit.restaurant_id);
+    visit["name"] = name;
+  });
 };
 </script>

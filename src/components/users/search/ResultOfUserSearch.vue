e54e4574ee790d5fd3a75406c192d93c98289bb4
David <David_cote_09@hotmail.com>
2023-04-23 20:37:15 -0400

    SearchBar dans USERS

---
+++ src/components/users/search/ResultOfUserSearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-table density="compact">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Rating</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in listeOfAllUsers" :key="index">
+          <td>{{ user.name }}</td>
+          <td>{{ user.rating }}</td>
+          <td><href> follow </href></td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
+</template>
+<script setup>
+import UserItem from "UserItem.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const props = defineProps({
+  listeOfAllUsers: Object,
+});
+</script>

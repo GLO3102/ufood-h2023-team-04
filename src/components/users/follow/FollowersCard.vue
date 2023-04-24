d3455e576b007c0211db48638abd20cfdf098fe8
= <=>
2023-04-23 20:38:19 -0400

    added feedback in follower and following tab

--- src/components/users/follow/FollowersCard.vue
+++ src/components/users/follow/FollowersCard.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div class="w-full">
+      <Loading :is-loading="!isLoaded" />
       <div
         class="mt-5 w-full flex flex-col items-center overflow-hidden text-sm"
       >
+        <p v-if="userInfos.length === 0">Nobody follows you</p>
         <a
           href="#"
           class="w-full text-gray-600 py-4 pl-6 pr-3 block hover:bg-gray-100 transition duration-150"
@@ -19,16 +21,19 @@
 <script setup>
 import { ref } from "vue";
 import Cookies from "js.cookie";
+import Loading from "../../navigation/Loading.vue";
 
 import { getUserInfoFollowers } from "../../../composables/useUser";
 
 const userInfos = ref([]);
 const token = Cookies.get("connectionToken");
+const isLoaded = ref(false);
 
 defineProps({ id: Object });
 
 const getInfos = async () => {
   userInfos.value = await getUserInfoFollowers(token.token, token.id);
+  isLoaded.value = true;
 };
 getInfos();
 </script>
--- src/components/users/follow/FollowingCard.vue
+++ src/components/users/follow/FollowingCard.vue
@@ -4,6 +4,8 @@
       <div
         class="mt-5 w-full flex flex-col items-center overflow-hidden text-sm"
       >
+        <Loading :is-loading="!isLoaded" />
+        <p v-if="userInfos.length === 0">You are following nobody</p>
         <a
           href="#"
           class="w-full text-gray-600 py-4 pl-6 pr-3 block hover:bg-gray-100 transition duration-150"
@@ -25,17 +27,19 @@
 <script setup>
 import { ref } from "vue";
 import Cookies from "js.cookie";
+import Loading from "../../navigation/Loading.vue";
 
 import {
   getUserInfoFollowing,
   deleteFollower,
 } from "../../../composables/useUser";
-
+const isLoaded = ref(false);
 const userInfos = ref([]);
 const token = Cookies.get("connectionToken");
 
 const getInfos = async () => {
   userInfos.value = await getUserInfoFollowing(token.token, token.id);
+  isLoaded.value = true;
 };
 
 const handleDeleteFollower = async (id) => {
